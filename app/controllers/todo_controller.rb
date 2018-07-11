a5f09afcb94caad4b864fb12a05b9effe21115eb
Kazu <kazugon115@gmail.com>
2018-07-11 17:44:45 +0000

    done

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,5 +4,13 @@ class TodoController < ApplicationController
     def show
         @todo_description = "Eat k"
         @todo_pomodoro_estimate = "0"
+        todo_id = params[:id]
+        if todo_id == '1'
+            @todo_description = "I'm changed" 
+            @todo_pomodoro_estimate = '100'
+        elsif todo_id == '5'
+            @todo_description = 'hello' 
+            @todo_pomodoro_estimate = '8'
+        end
     end
 end
